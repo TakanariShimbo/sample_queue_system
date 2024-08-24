5796644c3f3b6727bbcb40b4f8d93939e359142d
TakanariShimbo <takanari.shimbo@gmail.com>
2024-08-24 16:27:38 +0900

    modify

--- manager/sample_client.py
+++ manager/sample_client.py
@@ -12,7 +12,7 @@ FASTAPI_IP_ADDRESS = os.environ["FASTAPI_IP_ADDRESS"]
 FASTAPI_PORT = os.environ["FASTAPI_PORT"]
 
 
-def submit_embedding_request(texts: list[str], priority: Literal["high", "low"]) -> list[str]:
+def submit_job(texts: list[str], priority: Literal["high", "low"]) -> list[str]:
     data = []
     for text in texts:
         data.append({"text": text})
@@ -34,7 +34,7 @@ def submit_embedding_request(texts: list[str], priority: Literal["high", "low"])
         raise Exception(f"Failed to submit job: {response.text}")
 
 
-def get_job_result(job_id: str, priority: Literal["high", "low"]):
+def request_result(job_id: str, priority: Literal["high", "low"]):
     response = requests.get(
         url=f"http://{FASTAPI_IP_ADDRESS}:{FASTAPI_PORT}/get-result/{priority}-priority/{job_id}",
     )
@@ -51,28 +51,28 @@ def get_job_result(job_id: str, priority: Literal["high", "low"]):
         raise Exception(f"Failed to get job result: {response.text}")
 
 
-def process_job(job_id, priority: Literal["high", "low"]):
+def observe_submited_job(job_id, priority: Literal["high", "low"]):
     while True:
         time.sleep(2.5)
-        embdedding = get_job_result(job_id=job_id, priority=priority)
+        embdedding = request_result(job_id=job_id, priority=priority)
         if embdedding:
             break
 
 
 if __name__ == "__main__":
-    job_ids_low = submit_embedding_request(texts=[f"sample text", f"sample text"], priority="low")
-    job_ids_high = submit_embedding_request(texts=[f"sample text", f"sample text"], priority="high")
+    job_ids_low = submit_job(texts=[f"sample text", f"sample text"], priority="low")
+    job_ids_high = submit_job(texts=[f"sample text", f"sample text"], priority="high")
 
     threads_low = []
     for job_id in job_ids_low:
-        t = threading.Thread(target=process_job, args=(job_id, "low"))
+        t = threading.Thread(target=observe_submited_job, args=(job_id, "low"))
         t.start()
         threads_low.append(t)
         time.sleep(0.01)
 
     threads_high = []
     for job_id in job_ids_high:
-        t = threading.Thread(target=process_job, args=(job_id, "high"))
+        t = threading.Thread(target=observe_submited_job, args=(job_id, "high"))
         t.start()
         threads_high.append(t)
         time.sleep(0.01)
